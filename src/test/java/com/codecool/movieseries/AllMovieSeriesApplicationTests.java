a7f0aff4be5ce3de874349c4e37e18c1a3a52197
dr. Simon Bálint <balladin0@gmail.com>
2020-06-03 12:56:35 +0200

    add test2

--- src/test/java/com/codecool/movieseries/AllMovieSeriesApplicationTests.java
+++ src/test/java/com/codecool/movieseries/AllMovieSeriesApplicationTests.java
@@ -1,31 +1,53 @@
 package com.codecool.movieseries;
 
+import com.codecool.movieseries.entity.Episode;
+import com.codecool.movieseries.entity.Genre;
+import com.codecool.movieseries.repository.EpisodeRepository;
+import com.codecool.movieseries.repository.SeasonRepository;
 import com.codecool.movieseries.repository.SeriesRepository;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest
 @ExtendWith({SpringExtension.class})
 @DataJpaTest
 @ActiveProfiles("test")
 class AllMovieSeriesApplicationTests {
 
-    //    @Autowired
-//    private EpisodeRepository episodeRepository;
-//
-//    @Autowired
-//    private SeasonRepository seasonRepository;
+    @Autowired
+    private EpisodeRepository episodeRepository;
+
+    @Autowired
+    private SeasonRepository seasonRepository;
 
     @Autowired
     private SeriesRepository seriesRepository;
 
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
-    void contextLoads() {
+    public void saveOneEpisode() {
+        Episode GoTEp1 = Episode.builder()
+                .title("GoT Ep 1")
+                .genre(Lists.newArrayList(Genre.FANTASY))
+                .build();
+
+        episodeRepository.save(GoTEp1);
+
+        List<Episode> episodeList = episodeRepository.findAll();
+        assertThat(episodeList).hasSize(1);
     }
 
 }
