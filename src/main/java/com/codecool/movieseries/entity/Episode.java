f2f95c606549405cfae9ba77afed742190716274
dr. Simon Bálint <balladin0@gmail.com>
2020-06-03 08:52:59 +0200

    Initial commit

---
+++ src/main/java/com/codecool/movieseries/entity/Episode.java
@@ -0,0 +1,22 @@
+package com.codecool.movieseries.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Episode {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+}
---
+++ src/main/java/com/codecool/movieseries/repository/EpisodeRepository.java
@@ -0,0 +1,6 @@
+package com.codecool.movieseries.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EpisodeRepository extends JpaRepository {
+}
